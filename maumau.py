83a227799a7c0634d1f83e0785d65ade9828a8ad
Johannes Ritz <ritzj@student.ethz.ch>
2014-10-10 14:53:36 +0200

    Improved I/O.

--- maumau.py
+++ maumau.py
@@ -13,14 +13,14 @@ class Card(object):
 
     #To be printable in print(card)
     def __str__(self): 
-        return '{} {}'.format(self.color, self.value)
+        return '{} of {}'.format(self.color, self.value)
     #To be printable in print(listOfCards)
     def __repr__(self):
         return str(self)
 
 class Deck32(list):
-    VALUES = ["7","8","9","10","Bube", "Dame", "Koenig", "As"]
-    COLORS = ["Karo", "Herz", "Pik", "Kreuz"]
+    VALUES = ["7","8","9","10","Jack", "Queen", "King", "Ace"]
+    COLORS = ["diamonds", "hearts", "spades", "clubs"]
     
     def __init__(self):
         cards = []
@@ -44,14 +44,10 @@ class Player(object):
     
     def draw(self, deck):
         self.cards.append(deck.pop())
-        
-    #def playableCards(self, pile, deck):
-    #    pCards = []
-    #    for c in self.cards:
-    #        if c.isPlayable()
+
 
 class ComputerPlayer(Player):
-    #def __init__(self, name, deck):
+
     def play(self, pile, deck):
         nbrCards = len(self.cards)
         for c in self.cards:
@@ -77,6 +73,10 @@ class HumanPlayer(Player):
             print("No card can be played.")
             #print(self.cards[len(self.cards)-1], " is drawn.")
             print("{} is drawn.".format(self.cards[len(self.cards)-1] ))
+            try:
+                msg = input("Press Enter to continue. ")
+            except SyntaxError:
+                pass
         else:
         
             print("The pile's top card: {}".format(pile[len(pile)-1]))
@@ -84,8 +84,7 @@ class HumanPlayer(Player):
             print(self.cards)
             print("The following cards are playable: ")
             print(playableCards)
-            #TODO: I/O
-            cardNumber = input("Select a card by typing its number.")
+            cardNumber = input("Select a card by typing its number. ")
             c = playableCards[cardNumber]
             pile.append(c)
             self.cards.remove(c)
@@ -98,7 +97,6 @@ let the 'Mau Mau Game' begin
 """
 
 deck = Deck32()
-print(len(deck))
 pile = [deck.pop()]
 players = [ComputerPlayer("Computer1", deck),
               ComputerPlayer("Computer2", deck),
@@ -108,7 +106,7 @@ players = [ComputerPlayer("Computer1", deck),
 endOfGame = False
 while not endOfGame:
     for p in players:
-        print(p.name, "'s turn.")
+        print("{}'s turn.".format(p.name))
         #If the deck is empty, rebuild the deck
         if len(deck) == 0:
             while (len(pile) > 1):
@@ -118,7 +116,7 @@ while not endOfGame:
         p.play(pile, deck)
         if len(p.cards) == 0:
             endOfGame = True
-            print(p.name, "won the game.")
+            print("{} won the game.".format(p.name))
             #print(pile[0])
             break
         

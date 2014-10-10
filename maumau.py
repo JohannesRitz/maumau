1ca7bafacf055fb190fbbbffba34ebaece1e9aa9
Johannes Ritz <ritzj@student.ethz.ch>
2014-10-10 17:57:34 +0200

    Now the card 7 has the effect +2 cards.

--- maumau.py
+++ maumau.py
@@ -7,9 +7,13 @@ class Card(object):
     def __init__(self, color, value):
         self.color = color
         self.value = value
-
-    def isPlayable(self, other):
-        return self.color == other.color or self.value == other.value
+    
+    #Can this card be played on top of the other card?
+    def isPlayable(self, other, game):
+        if game.sevenCount == 0:
+            return self.color == other.color or self.value == other.value
+        else:
+            return self.value == "7"
 
     #To be printable in print(card)
     def __str__(self): 
@@ -18,8 +22,8 @@ class Card(object):
     def __repr__(self):
         return str(self)
 
-class Deck32(list):
-    VALUES = ["7","8","9","10","Jack", "Queen", "King", "Ace"]
+class Deck36(list):
+    VALUES = ["6","7","8","9","10","Jack", "Queen", "King", "Ace"]
     COLORS = ["diamonds", "hearts", "spades", "clubs"]
     
     def __init__(self):
@@ -51,42 +55,52 @@ class Player(object):
             print("New deck.")
         self.cards.append(deck.pop())
         
-    def playCard(self, card, pile):
+    def playCard(self, card, pile, game):
         pile.append(card)
         self.cards.remove(card)
-        
+        game.effectActive = (card.value == "8" or card.value == "7")
+        if card.value == "7":
+            game.sevenCount += 1
 
 class ComputerPlayer(Player):
 
     def play(self, pile, deck, game):
         nbrCards = len(self.cards)
         for c in self.cards:
-            if c.isPlayable(pile[len(pile)-1]):
-                self.playCard(c, pile)
-                game.effectActive = (c.value == "8")
+            if c.isPlayable(pile[len(pile)-1], game):
+                self.playCard(c, pile, game)
                 #print(c, "is played by ", player.name)
                 break
         if nbrCards == len(self.cards): #no card was played
-            self.draw(pile, deck)
-            print("Need to draw.")
+            if game.sevenCount > 0:
+                for times in range(2*game.sevenCount):
+                    self.draw(pile, deck)
+                game.sevenCount = 0
+            else:
+                self.draw(pile, deck)
+                print("Need to draw.")
 
 class HumanPlayer(Player):
     
     def play(self, pile, deck, game):
         playableCards = []
         for c in self.cards:
-            if c.isPlayable(pile[len(pile)-1]):
+            if c.isPlayable(pile[len(pile)-1], game):
                 playableCards.append(c)
                 
         if (len(playableCards) == 0):
-            self.draw(pile, deck)
             print("No card can be played.")
-            #print(self.cards[len(self.cards)-1], " is drawn.")
-            print("{} is drawn.".format(self.cards[len(self.cards)-1] ))
-            try:
-                input("Press Enter to continue. ")
-            except SyntaxError:
-                pass
+            if game.sevenCount > 0:
+                for times in range(2*game.sevenCount):
+                    self.draw(pile, deck)
+                game.sevenCount = 0
+            else:
+                self.draw(pile, deck)
+                #print("{} is drawn.".format(self.cards[len(self.cards)-1] ))
+                #try:
+                #    input("Press Enter to continue. ")
+                #except SyntaxError:
+                #    pass
         else:
         
             print("The pile's top card: {}".format(pile[len(pile)-1]))
@@ -96,15 +110,13 @@ class HumanPlayer(Player):
             print(playableCards)
             cardNumber = input("Select a card by typing its number. ")
             c = playableCards[cardNumber]
-            game.effectActive = (c.value == "8")
-            self.playCard(c, pile)
+            self.playCard(c, pile, game)
 
 class Game:
-    effectActive = False
-    endOfGame = False
     def __init__(self):
-        effectActive = False
-        endOfGame = False
+        self.effectActive = False
+        self.endOfGame = False
+        self.sevenCount = 0
 
 
 """
@@ -115,7 +127,7 @@ let the 'Mau Mau Game' begin
 #nbrHumans = input("How many humans are playing? ")
 #TODO: increase the deck size according to player numbers
 
-deck = Deck32()
+deck = Deck36()
 pile = [deck.pop()]
 players = [ComputerPlayer("Computer1", deck),
               ComputerPlayer("Computer2", deck),
@@ -127,7 +139,7 @@ game = Game()
 while not game.endOfGame:
     for p in players:
         #Must the player pause due to an 8?
-        print("{}'s turn.".format(p.name))
+        print("{}'s turn. {} cards.".format(p.name,len(p.cards)))
         if game.effectActive and pile[len(pile)-1].value == "8":
             game.effectActive = False
             print('{} must pause.'.format(p.name)) 
@@ -136,6 +148,5 @@ while not game.endOfGame:
         if len(p.cards) == 0:
             game.endOfGame = True
             print("{} won the game.".format(p.name))
-            #print(pile[0])
             break
         
